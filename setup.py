060bc31f86d7b4597fb7fdb820b3e799ae163a0e
Russ <russf@topia.com>
2012-06-16 18:32:18 +0100

    Added Google Spreadsheet dependency

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ messaging or social actions""",
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'gspread',
       ],
       entry_points="""
       # -*- Entry points: -*-
