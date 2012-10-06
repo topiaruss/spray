fb915b15e3c43c6a2806073604e062e5c0e0ffd2
Russ <russf@topia.com>
2012-10-06 20:38:20 +0100

    added venv activate

--- bin/run_sprayd.sh
+++ bin/run_sprayd.sh
@@ -2,5 +2,9 @@
 (
  flock -n 9 || exit 1
   echo 'starting bin/sprayd'
+  cd ~/spray
+  source bin/activate
+  cd spray
   bin/sprayd >> /tmp/cron_sprayd.log
+  echo 'ending bin/sprayd'
 ) 9>/var/lock/cron_sprayd
